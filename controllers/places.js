4ad3c82dac4574b179cf0eea056aa4b88b81b99d
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-12-18 00:08:32 -0500

    fixed errors

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,10 @@
-const router = require('express').Router().all
+const express = require('express')
+const router = express.Router()
 
 //static code here
+router.get('/', (req,res)=> {
+    res.send('GET/places')
+ //Make the first route in places.js. We can use the index page (i.e., the GET route that will eventually show a list of all places).
+})
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('./places', require('./controllers/places'))
+
 
 app.get('/',(req,res) => {
     res.send('Hello World')
 })
+
+
+app.use('/places', require('./controllers/places'))
 
 app.get('*', (req,res)=> {
     res.send('<h1>This a 404 page, the link you have entered does not exist.</h1>')
