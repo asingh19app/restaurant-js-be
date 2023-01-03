8c0f6b978c3a938cf970a36870366efef8ebde22
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2023-01-03 13:20:12 -0500

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,37 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const places = require('../models/places.js')
 
 
 //static code here
 router.get('/', (req,res)=> {
-    let places = [
-        {
-            name:'Indian Food Place',
-            city:'Mohali',
-            state:'Panjab',
-            cuisines:'Chicken Tikka',
-            image:'/images/indianFood.jpg'
-
-        },
-
-        {
-            name:'Chinese Food Place',
-            city:'Yonkers',
-            state:'New York',
-            cuisines:'Orange Chicken',
-            image:'/images/orangeChicken.jpg'
-        },
-        {
-            name:'Burger Food Place',
-            city:'Bronx',
-            state:'New York',
-            cuisines:'Burger',
-            image:'/images/burgerImage.webp'
-        }
-    ]
     res.render('places/index', {places})
-    //Pass the places array into the render method as shown below. This will allow us to use that array inside of our view. We're one step closer to showing this data to our user!
- //The one additional change here is that since we have a folder inside(places) of our views folder, we do need to reference that folder. Therefore, your index route inside of places.js should look like this:
+
 })
 
 //NEW
@@ -40,10 +15,21 @@ router.get('/new', (req,res)=> {
 })
 
 //CREATE
-router.post('/', (req,res) => {
-    console.log(req.body)
-    res.send('POST/places')
-})
+router.post('/', (req, res) => {
+   // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
   
 
 
---
+++ models/places.js
@@ -0,0 +1,25 @@
+module.exports =  [
+    {
+        name:'Indian Food Place',
+        city:'Mohali',
+        state:'Panjab',
+        cuisines:'Chicken Tikka',
+        image:'/images/indianFood.jpg'
+
+    },
+
+    {
+        name:'Chinese Food Place',
+        city:'Yonkers',
+        state:'New York',
+        cuisines:'Orange Chicken',
+        image:'/images/orangeChicken.jpg'
+    },
+    {
+        name:'Burger Food Place',
+        city:'Bronx',
+        state:'New York',
+        cuisines:'Burger',
+        image:'/images/burgerImage.webp'
+    }
+]
