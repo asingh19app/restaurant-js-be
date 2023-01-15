7c78ad54f7f6c0b60123490a7cffae464aca6394
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2023-01-15 11:12:39 -0500

    added reviewed show page

--- models/places.js
+++ models/places.js
@@ -5,13 +5,17 @@ const {Schema} = mongoose
 //placeSchema 
 const placeSchema = new Schema({
 name: {type: String, required: true},
-image: {String},
+image: String,
 cuisines: {type:String, required: true},
 city: {type:String, default: 'Anytown'},
 state: {type:String, default: 'USA'},
 founded: Number
 
 })
+
+placeSchema.methods.showEstablished =  function() {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}`
+}
 
 
 const Place = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,24 +4,25 @@ const Default = require('../default')
 function Show(data) {
     return(
         <Default>
-            <main>
-                <h1>{data.place.name}</h1>
-                <div>  
-                    <h2>Rating</h2> 
-                    <p>Not rated</p>
-                </div>
-
-                <div>
-                    <h2>Description</h2>
-                    <p> Located in {data.place.city} {data.place.state}</p>
-                    <p>Speciality Dish: {data.place.cuisines}</p>
-                </div>
-
-                
-                   <img src= {data.place.image}  alt={data.place.cuisines} height = {500} width = {500}/>
-                
-                 
-                    <div className='buttons' >
+  <main>
+    <div className="row">
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+    
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
               <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                     Edit
                 </a>
@@ -29,14 +30,15 @@ function Show(data) {
              <form method ="POST" action = {`/places/${data.id}?_method=DELETE`} >
                 <button type='submit' className='btn btn-danger'>DELETE</button>
              </form>
-             
-             </div>
-                
+    </div>
+    </div>
+  </main>
 
-              
-            </main>
         </Default>
     )
 }
 
+
+
+                
 module.exports = Show
